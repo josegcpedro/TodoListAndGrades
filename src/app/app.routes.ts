4d4bbf2c7f8265802676fa6c93865b8cca1bba21
Jose <pedrojcunha7@hotmail.com>
2025-02-12 09:26:37 +0100

    feat: create home page and add routes to todoList and grades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "todolist",
+        component: HomeComponent
+    },
+    {
+        path: "notes",
+        component: HomeComponent
+    },
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  constructor(private router: Router) { }
+
+  goTo(page: string) {
+    this.router.navigate([page]); // Redirige vers '/todolist' ou '/notes'
+  }
+}
